d4b734938e03758129986f17a475d6255294b607
LucasCzerny <czernylucas@gmail.com>
2023-01-09 10:46:35 +0100

    Navbar now collapses if a link is clicked or if the content area is clicked

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-let header = document.querySelector("nav");
+const header = document.querySelector("nav");
 
-header.addEventListener('show.bs.collapse', function () {
+header.addEventListener("show.bs.collapse", function () {
     header.classList.toggle("nav-expanded-style");
 });
 
-header.addEventListener('hide.bs.collapse', function () {
+header.addEventListener("hide.bs.collapse", function () {
     setTimeout(function() {
         header.classList.toggle("nav-expanded-style");
     }, 225);
+});
+
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("navbarSupportedContent");
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", function() {
+        const navBarCollapse = document.querySelector(".navbar-collapse");
+
+        if (navBarCollapse.classList.contains("show")) {
+            bsCollapse.toggle();
+        }
+    })
+});
+
+document.addEventListener("click", function() {
+    const navBarCollapse = document.querySelector(".navbar-collapse");
+
+    if (navBarCollapse.classList.contains("show")) {
+        bsCollapse.toggle();
+    }
 });
