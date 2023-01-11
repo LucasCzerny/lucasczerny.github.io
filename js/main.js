d0a3cd81c3702c99fdcad3ac0c1430af90ffc0b3
LucasCzerny <czernylucas@gmail.com>
2023-01-11 14:54:44 +0100

    Improved user-friendliness

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+const NR_OF_SECTIONS = 4;
+let sectionHeights = [];
+
+function setSectionHeights() {
+    const sections = document.querySelectorAll("main, section");
+    
+    for (let i = 0; i < NR_OF_SECTIONS; i++) {
+        const section = sections[i];
+        sectionHeights[i] = section.offsetHeight;
+    }
+}
+
+function changeActiveLink() {
+    //                          Firefox                               Chrome/Chromium
+    let currentScrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    let relativeHeight = currentScrollPosition;
+    
+    let navLinks = document.querySelectorAll(".nav-link");
+
+    for (let i = 0; i < NR_OF_SECTIONS; i++) {
+        let height = sectionHeights[i];
+
+        if (relativeHeight <= height) {
+            navLinks.forEach(function(link) {
+                link.classList.remove("active");
+            });
+
+            navLinks[i].classList.add("active");
+
+            return;
+        }
+
+        relativeHeight -= height;
+    }
+}
+
+window.addEventListener("load", () => {
+    setSectionHeights();
+    changeActiveLink();
+});
+
+window.addEventListener("resize", setSectionHeights);
+window.addEventListener("scroll", changeActiveLink);
+
 const header = document.querySelector("nav");
 
 header.addEventListener("show.bs.collapse", function () {
